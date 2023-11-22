40241faae7a0914532c80dcc5d42ddced4033045
Sindhu-Peddi <147684620+Sindhu-Peddi@users.noreply.github.com>
2023-11-22 19:48:52 +0530

    Add files via upload

---
+++ stock/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import StockPage from './StockPage';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <StockPage/>
+    </div>
+  );
+}
+
+export default App;
